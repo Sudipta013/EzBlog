5eae94b4a3dce0cc7e80b14528e09634507007d3
Sudipta paul <Sudiptopaul403@gmail.com>
2024-05-10 19:35:06 +0530

    created getusers api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Post from "../models/post.model.js";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs';
@@ -65,6 +66,49 @@ export const deleteUser = async (req, res, next) => {
 export const signout = async (req, res, next) => {
     try {
         res.clearCookie('access_token').status(200).json('User had been signed out');
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getUsers = async (req, res, next) => {
+    if(!req.user.isAdmin){
+        return next(errorHandler(403, 'You are not allowed to see all users'));
+    }
+    try {
+       const startIndex = parseInt(req.query.startIndex) || 0;
+       const limit = parseInt(req.query.limit) || 9;
+       const sortDirection = req.query.sort === 'asc' ? 1 : -1;
+        
+       const users = await User.find()
+        .sort({createdAt: sortDirection})
+        .skip(startIndex)
+        .limit(limit);
+
+        const usersWithoutPassword = users.map((user) => {
+            const {password, ...rest} = user._doc;
+            return rest;
+        });
+
+        const totalUsers = await User.countDocuments();
+
+        const now = new Date();
+
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        );
+
+        const lastMonthUsers = await Post.countDocuments({
+            createdAt: {$gte: oneMonthAgo},
+        });
+
+        res.status(200).json({
+            users: usersWithoutPassword,
+            totalUsers,
+            lastMonthUsers,
+        });
     } catch (error) {
         next(error);
     }
