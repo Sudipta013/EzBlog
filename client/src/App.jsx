04502c60664767de845d5d637a2655214d558227
Sudipta paul <Sudiptopaul403@gmail.com>
2024-05-15 23:52:41 +0530

    complete postpage functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from './components/PrivateRoute'
 import OnlyAdminPrivateRoute from './components/OnlyAdminCreatePost'
 import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/UpdatePost'
+import PostPage from './pages/PostPage'
 
 
 export default function App() {
@@ -24,6 +25,7 @@ export default function App() {
           <Route path='/dashboard' element= {<Dashboard/>}></Route>
         </Route>
         <Route path='/projects' element= {<Projects/>}></Route>
+        <Route path='/post/:postSlug' element= {<PostPage/>}></Route>
         <Route path='/sign-in' element= {<SignIn/>}></Route>
         <Route path='/sign-up' element= {<SignUp/>}></Route>
         <Route element={<OnlyAdminPrivateRoute/>}>
